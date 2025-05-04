f66a93703508ea3be59d6033ae72f96b10ff3449
Hugaleno Bezerra <hugaleno@gmail.com>
2025-05-04 20:46:17 -0300

    fix tests and upload/download version

--- src/sum.test.js
+++ src/sum.test.js
@@ -3,15 +3,3 @@ const soma = require('./sum')
 test("A soma de 1 + 1 deve ser 2", () => {
   expect(soma(1,1)).toBe(2)
 });
-
-test("A sub de 1 - 1 deve ser 0", () => {
-  expect(soma(1,1)).toBe(0)
-});
-
-test("A mult de 1 * 1 deve ser 1", () => {
-  expect(soma(1,1)).toBe(1)
-});
-
-test("A div de 1 / 1 deve ser 1", () => {
-  expect(soma(1,1)).toBe(1)
-});
